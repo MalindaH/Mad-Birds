06f504450f5af9438123470e2981522437f69484
Malinda <linhui.huang@mail.mcgill.ca>
2021-01-10 19:01:50 -0500

    added main menu and settings menu

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -8,29 +8,63 @@ public class LevelLoader : MonoBehaviour
     public Animator transition;
     public float transitionTime = 1f;
 
+    private static int _nextLevelIndex = 1; // static: same variable for all levels
+    private Enemy[] _enemies;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    private void Start() 
+    {
+        Debug.Log("start: "+transition);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            LoadNextLevel();
+        foreach (Enemy enemy in _enemies)
+        {
+            if (enemy != null) return;
         }
+
+        LoadNextLevel();
+
+        // if (Input.GetMouseButtonDown(0)) {
+        //     LoadNextLevel();
+        // }
     }
 
     public void LoadNextLevel() {
-
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
 
-        // StartCoroutine(LevelController.MyCoroutine());
+        // StartCoroutine(MyCoroutine());
     }
 
     IEnumerator LoadLevel(int levelIndex) {
-        // play animation
-        transition.SetTrigger("Start");
+        Debug.Log("load level: "+transition+levelIndex);
+        // transition.gameObject.SetActive(true);
+        transition.SetTrigger("Start"); // play animation
 
         yield return new WaitForSeconds(transitionTime);
 
         if (levelIndex < SceneManager.sceneCountInBuildSettings) {
             SceneManager.LoadScene(levelIndex);
+        } else {
+            SceneManager.LoadScene("Menu");
         }
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        Debug.Log("load level: "+transition);
+        transition.SetTrigger("Start"); // play animation
+       
+        yield return new WaitForSeconds(transitionTime);
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
     }
 }
---
+++ Assets/MenuSettings.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class MenuSettings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start() {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for(int i = 0; i<resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume) { // dynamic float
+        audioMixer.SetFloat("volumeParam", volume);
+    }
+
+    public void SetQuality(int qualityIndex) {
+        // Debug.Log(5 - qualityIndex);
+        QualitySettings.SetQualityLevel(5 - qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen) {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, Screen.fullScreen);
+    }
+}
